f64c60770990592bae99ae9556d11c87826f9b4d
sarahhlandis <sarahlandis95@gmail.com>
2023-07-11 15:21:05 +1000

    feat: minor styling added to nav links in nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ export function Nav({isLoggedIn}) {
   const location = useLocation();
   const isMdScreenOrLarger = useMediaQuery({ minWidth: 768 });
 
+  const linkStyle = "hover:scale-105 transition-transform duration-300"
   const isHomePage = location.pathname === "/";
 
   return (
@@ -14,34 +15,34 @@ export function Nav({isLoggedIn}) {
         {/* on med/lg screens, show only report a missing link and/or home link */}
         {isMdScreenOrLarger ? (
             <div className="">
-            <ul className="text-3xl flex flex-row px-5">
-                {/* Show home link when not on homepage */}
-                {!isHomePage && (
-                  <li className="mr-10">
-                    <NavLink to="/">Home</NavLink>
-                  </li>
-                )}
+                <ul className="text-3xl flex flex-row px-5">
+                    {/* Show home link when not on homepage */}
+                    {!isHomePage && (
+                    <li className={`mr-10 ${linkStyle}`}>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    )}
 
-                {/* if user is logged in, show dashboard, logout, and/or home link from above */}
-                {isLoggedIn ? (
-            <>
-                <li className="mr-10">
-                  <NavLink to="/dashboard">Dashboard</NavLink>
+                    {/* if user is logged in, show dashboard, logout, and/or home link from above */}
+                    {isLoggedIn ? (
+                <>
+                    <li className={`mr-10 ${linkStyle}`}>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                    </li>
+                    {/* add adjustment to appropriately log user out but direct to homepage 
+                    need to wait until login functionality is coded */}
+                    <li className={`${linkStyle}`}>
+                    <NavLink to="/">Logout</NavLink>
+                    </li>
+                </>
+                ) : (
+                    // else if user is a public user, show below link
+                    // /report route directs to page with login and signup forms
+                <li className={`${linkStyle}`}>
+                    <NavLink to="/report">Report a Missing Person</NavLink>
                 </li>
-                {/* add adjustment to appropriately log user out but direct to homepage 
-                need to wait until login functionality is coded */}
-                <li>
-                  <NavLink to="/">Logout</NavLink>
-                </li>
-              </>
-            ) : (
-                // else if user is a public user, show below link
-                // /report route directs to page with login and signup forms
-              <li>
-                <NavLink to="/report">Report a Missing Person</NavLink>
-              </li>
-            )}
-            </ul>
+                )}
+                </ul>
             </div>
         ) : (
         // on small screens, use hamburger menu instead of horizontal nav link options
