f52633e32c74a00658a3dd17544218a611e6df67
sarahhlandis <sarahlandis95@gmail.com>
2023-07-26 13:58:19 +1000

    feat: mobile styling complete based off mock data array.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -250,40 +250,71 @@ export function Home() {
   }, []);
 
 
-return (
+  return (
     <div>
       <div className="mt-16 flex justify-center text-center font-main font-bold text-2xl md:text-3xl">
         Currently Active Reports
       </div>
-      <div className="p-4 mt-5 lg:p-2 gap-4 md:space-x-5 w-full flex flex-wrap justify-center">
-        {reports.map((report) => (
-          <div className="flex flex-row p-2" key={report.reportId}>
-            <div className="mt-5">
-              <div className="flex flex-col">
-                <div className="aspect-w-1 aspect-h-1">
-                  <img src={report.photoURL} alt="Missing Person" className="h-64 w-64 object-cover" />
+      {isMdScreenOrLarger ? (
+        <div className="p-4 mt-5 lg:p-2 gap-4 md:space-x-5 w-full flex flex-wrap justify-center">
+          {reports.map((report) => (
+            <div className="flex flex-row p-2" key={report.reportId}>
+              <div className="mt-5">
+                <div className="flex flex-col">
+                  <div className="aspect-w-1 aspect-h-1">
+                    <img src={report.photoURL} alt="Missing Person" className="md:h-52 md:w-52 lg:h-64 lg:w-64 object-cover" />
+                  </div>
+                  <div className="flex flex-col font-main">
+                    <p className="text-2xl pt-1">{report.fullName}</p>
+                    <p>{report.ageNumber} {report.ageMeasurement} old</p>
+                    <p>Last Seen: {new Date(report.dateLastSeen).toISOString().split("T")[0]}</p>
+                    <p>Location Last Seen: {report.locationLastSeen.address}</p>
+                  </div>
                 </div>
-                <div className="flex flex-col font-main">
-                  <p className="text-2xl">{report.fullName}</p>
-                  <p>{report.ageNumber} {report.ageMeasurement} old</p>
-                  <p>Last Seen: {new Date(report.dateLastSeen).toISOString().split("T")[0]}</p>
-                  <p>Location Last Seen: {report.locationLastSeen.address}</p>
+                <div className="flex space-x-4 pt-2 w-full justify-end">
+                  <button
+                    className="bg-yellow rounded-full p-1 text-sm"
+                    onClick={() => handleEditReportButtonClick(report.reportId)}
+                  >
+                    edit
+                  </button>
                 </div>
-              </div>
-              <div className="flex space-x-4 pt-2 w-full justify-end">
-                <button
-                  className="bg-yellow rounded-full p-1 " // Corrected classname to className
-                  onClick={() => handleEditReportButtonClick(report.reportId)}
-                >
-                  edit
-                </button>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div className="p-4 mt-5 w-full flex flex-wrap justify-center">
+          {reports.map((report) => (
+            <div className="flex flex-row p-2" key={report.reportId}>
+              <div className="">
+                <div className="flex flex-row">
+                  <div className="aspect-w-1 aspect-h-1 flex items-center">
+                    <img src={report.photoURL} alt="Missing Person" className="h-28 w-28 object-cover" />
+                  </div>
+                  <div className="flex flex-col font-main text-sm pl-2">
+                    <p className="text-xl">{report.fullName}</p>
+                    <p>{report.ageNumber} {report.ageMeasurement} old</p>
+                    <p>Last Seen: {new Date(report.dateLastSeen).toISOString().split("T")[0]}</p>
+                    <p>Location Last Seen: {report.locationLastSeen.address}</p>
+                  </div>
+                </div>
+                <div className="flex w-full justify-end">
+                  <button
+                    className="bg-yellow rounded-full p-1 text-xs"
+                    onClick={() => handleEditReportButtonClick(report.reportId)}
+                  >
+                    edit
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}  
-
+}
+  
 export default Home;
+
