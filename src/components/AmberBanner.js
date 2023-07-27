9fa2e579b10ef45f692ec809448df6d22b8da733
sarahhlandis <sarahlandis95@gmail.com>
2023-07-27 14:20:14 +1000

    feat!: Added conditional rendering for when the component mounts.

--- src/components/AmberBanner.js
+++ src/components/AmberBanner.js
@@ -29,26 +29,37 @@ function AmberAlertBanner() {
   return (
     <div className="h-12 bg-amber flex justify-end overflow-hidden">
       <div
-        className={`w-full flex items-center whitespace-nowrap ${
-          startAnimation ? "animate-continuous" : ""
-        } justify-evenly font-main`}
+        className={`w-full flex items-center whitespace-nowrap font-main ${
+          startAnimation ? "animate-continuous justify-evenly" : "justify-center"
+        }`}
       >
+        {/* If there's an error, just render the error in the banner with no animation */}
         {error ? (
           <div className="font-bold uppercase">{error}</div>
         ) : (
           <>
-            <div className="font-main font-bold uppercase">Active Amber Alerts</div>
-            {amberAlerts.map((alert) => (
-              <div className="flex flex-row uppercase" key={alert._id}>
-                <span>{alert.fullName},</span>
-                <span className="ml-1">
-                  {alert.currentAge[0].number} {alert.currentAge[0].type} old,
-                </span>
-                <span className="ml-1">
-                  {alert.locationLastSeen.city}, {alert.locationLastSeen.state}
-                </span>
-              </div>
-            ))}
+          {/* When startanimation is false, just render the 'heading' so users know 
+          the purpose of this section */}
+            {!startAnimation && (
+              <div className="font-main font-bold uppercase">Active Amber Alerts</div>
+            )}
+            {/* When start animation is true, pull info from the endpoint and render */}
+            {startAnimation && (
+              <>
+                <div className="font-main font-bold uppercase mr-4">Active Amber Alerts</div>
+                {amberAlerts.map((alert) => (
+                  <div className="flex flex-row uppercase" key={alert._id}>
+                    <span>{alert.fullName},</span>
+                    <span className="ml-1">
+                      {alert.currentAge[0].number} {alert.currentAge[0].type} old,
+                    </span>
+                    <span className="ml-1">
+                      {alert.locationLastSeen.city}, {alert.locationLastSeen.state}
+                    </span>
+                  </div>
+                ))}
+              </>
+            )}
           </>
         )}
       </div>
@@ -57,4 +68,3 @@ function AmberAlertBanner() {
 }
 
 export default AmberAlertBanner;
-
